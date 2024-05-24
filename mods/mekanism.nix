3bf99ba70756317f99e912d2f11e9690ce8046c6
k6av <43968691+k6av@users.noreply.github.com>
2024-05-24 13:32:25 +0200

    refactor: change Mekanism to download from GitHub

--- mods/mekanism.nix
+++ mods/mekanism.nix
@@ -1,18 +1,18 @@
 { pkgs, ... }: [
   (pkgs.fetchurl { # Mekanism
-    url = "https://mediafilez.forgecdn.net/files/5125/665/Mekanism-1.20.1-10.4.6.20.jar";
-    hash = "sha256-44ow342wApqFE/ewFbG/ExLjWluzalSpkw48a4S3ZA8=";
+    url = "https://github.com/mekanism/Mekanism/releases/download/v1.20.1-10.4.6.20/Mekanism-1.20.1-10.4.6.20.jar";
+    hash = "sha256-bIvnmlS+9TjTKAm/K8kMHY+g5jV9trtPP5mGPYhPCQo=";
   })
   (pkgs.fetchurl { # Mekanism Tools
-    url = "https://mediafilez.forgecdn.net/files/5125/669/MekanismTools-1.20.1-10.4.6.20.jar";
-    hash = "sha256-ZIt7cvmpjhgAq8zzsRFvNhHy3Nx4QKZ3JisvJ0B5XUs=";
+    url = "https://github.com/mekanism/Mekanism/releases/download/v1.20.1-10.4.6.20/MekanismTools-1.20.1-10.4.6.20.jar";
+    hash = "sha256-7Hel2gb088gCyc+8nhQoGs+4UPL9Fc1VPYPdXhB5OKA=";
   })
   (pkgs.fetchurl { # Mekanism Generators
-    url = "https://mediafilez.forgecdn.net/files/5125/668/MekanismGenerators-1.20.1-10.4.6.20.jar";
-    hash = "sha256-yVKnLCsakuekZI0W4QZkCtP3fwmxrTY+hIM3HXhJ+wQ=";
+    url = "https://github.com/mekanism/Mekanism/releases/download/v1.20.1-10.4.6.20/MekanismGenerators-1.20.1-10.4.6.20.jar";
+    hash = "sha256-4W+V/6urv+9GYsmwZmQy3Vk8W1KC91Tbt7Fy1slFNQU=";
   })
   (pkgs.fetchurl { # Mekanism Additions
-    url = "https://mediafilez.forgecdn.net/files/5125/667/MekanismAdditions-1.20.1-10.4.6.20.jar";
-    hash = "sha256-xItDvw83srtAFZQ/irm87UoLAziNWVAHdj0j5tK0sEc=";
+    url = "https://github.com/mekanism/Mekanism/releases/download/v1.20.1-10.4.6.20/MekanismAdditions-1.20.1-10.4.6.20.jar";
+    hash = "sha256-zInIqFvbcm00q4bedqqlz6QKWuHmjHjYpJ+CLHbGmmU=";
   })
 ]
